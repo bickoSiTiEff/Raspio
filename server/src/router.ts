83c285999cf80accc8ce7e581b1a82a6026ffdbb
Fabian <fabian.paul.gurtner@gmail.com>
2021-05-30 23:10:41 +0200

    Add REST server stub
    The server validates requests against the defined API specification so that invalid requests or responses will fail.
    
    #RASPIO-4 state To Verify

---
+++ server/src/router.ts
@@ -0,0 +1,15 @@
+import {Router} from "express";
+
+import transmission from "./routes/transmission";
+import library from "./routes/library";
+import playlist from "./routes/playlist";
+import playback from "./routes/playback";
+
+const router = Router();
+
+router.use("transmission", transmission);
+router.use("songs", library);
+router.use("playlist", playlist);
+router.use("playback", playback);
+
+export default router;
---
+++ server/src/server.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import * as OpenApiValidator from "express-openapi-validator";
+import {json} from "body-parser";
+import router from "./router";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(json());
+
+app.use(
+	OpenApiValidator.middleware({
+		apiSpec: process.env.API_SPEC || "../api-specification.yaml",
+		validateApiSpec: true,
+		validateRequests: true,
+		validateResponses: true
+	})
+);
+
+app.use(router);
+
+app.use((err: any, req: express.Request, res: express.Response, _next: express.NextFunction) => {
+	console.error(err);
+	res.status(err.status || 500).json({
+		message: err.message,
+		errors: err.errors
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}!`);
+});
