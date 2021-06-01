b36854f8764d76603ba2fad1a61fe84eb069c379
Fabian <fabian.paul.gurtner@gmail.com>
2021-06-01 12:38:53 +0200

    Implement library mock server

--- server/src/mockRoutes/library.ts
+++ server/src/mockRoutes/library.ts
@@ -1,6 +1,23 @@
 import {Router} from "express";
+import {Song} from "../models/Song";
 
 const library = Router();
 
+library.get("/", (req, res) => {
+	res.send([
+		{
+			path: "stefan-rauch/15er-steyr.mp3",
+			title: "15er Steyr",
+			artist: "Stefan Rauch",
+			duration: 192
+		},
+		{
+			path: "sääftig/dirndl-weed.wav",
+			title: "Dirndl Weed",
+			artist: "SÄÄFTIG",
+			duration: 132
+		}
+	] as Song[]);
+})
 
 export default library;
