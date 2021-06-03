7ec5930b5a13c3cc463c4d2ec1d9fc674a6c55bd
Fabian <fabian.paul.gurtner@gmail.com>
2021-06-03 19:20:22 +0200

    Implement API handler for MPD library

--- server/src/routes/library.ts
+++ server/src/routes/library.ts
@@ -1,7 +1,28 @@
 import Router from "express-promise-router";
+import {MPC, Song} from "mpc-js";
+import * as API from "../models/Song";
 
 export default async () => {
 	const library = Router();
+
+	const mpc = new MPC();
+	await mpc.connectTCP(process.env.MPD_HOST || "localhost", Number(process.env.MPD_PORT || 6600));
+
+	library.get("/", async (req, res) => {
+		const database = await mpc.database.listAllInfo();
+		
+		const songs = database
+			.filter(entry => entry.isSong())
+			.map(i => i as Song) // This has been checked in the previous step, but the compiler is dumb
+			.map((song: Song) => ({
+				path: song.path,
+				title: song.title,
+				artist: song.artist || song.composer || song.performer || "Unknown",
+				duration: song.duration
+			} as API.Song));
+
+		res.send(songs);
+	});
 
 	return library;
 };
