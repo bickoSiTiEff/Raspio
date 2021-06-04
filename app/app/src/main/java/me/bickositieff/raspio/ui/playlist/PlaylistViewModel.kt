4c81ae7e667fa37de0d909662aa4f76cb454a781
CallMeAlasca <50996839+CallMeAlasca@users.noreply.github.com>
2021-06-05 00:20:50 +0200

    Fixing fragment mess-up

--- app/app/src/main/java/me/bickositieff/raspio/ui/playlist/PlaylistViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/playlist/PlaylistViewModel.kt
@@ -1,10 +1,10 @@
-package me.bickositieff.raspio.ui.home
+package me.bickositieff.raspio.ui.playlist
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class PlaylistViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
