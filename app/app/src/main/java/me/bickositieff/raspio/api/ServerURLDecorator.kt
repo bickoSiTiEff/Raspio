7ba331fab1eaa08ee58b281f7289a9fc7a0e5382
SchneckchenAndy <aigner.andi05@gmail.com>
2021-06-05 17:27:06 +0200

    Move kgen files to subpackage generated

--- app/app/src/main/java/me/bickositieff/raspio/api/ServerURLDecorator.kt
+++ app/app/src/main/java/me/bickositieff/raspio/api/ServerURLDecorator.kt
@@ -1,6 +1,6 @@
 package me.bickositieff.raspio.api
 
-import me.bickositieff.raspio.ApiDecorator
+import me.bickositieff.raspio.generated.ApiDecorator
 import retrofit2.Retrofit
 
 class ServerURLDecorator(private val url: String) : ApiDecorator {
