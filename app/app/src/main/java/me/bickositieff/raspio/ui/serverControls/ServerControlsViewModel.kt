ee4f56d0c7477c8b259588e16d1be59c5763e9de
SchneckchenAndy <aigner.andi05@gmail.com>
2021-06-06 19:56:23 +0200

    Implement changeTransmissionState in management ViewModel

--- app/app/src/main/java/me/bickositieff/raspio/ui/serverControls/ServerControlsViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/serverControls/ServerControlsViewModel.kt
@@ -1,9 +1,14 @@
 package me.bickositieff.raspio.ui.serverControls
 
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import me.bickositieff.raspio.generated.api.TransmissionApi
 
-class ServerControlsViewModel : ViewModel() {
+class ServerControlsViewModel(application: Application) : AndroidViewModel(application) {
 
     val frequency = MutableLiveData<String>().apply {
         value = "88"
@@ -12,7 +17,17 @@ class ServerControlsViewModel : ViewModel() {
     val transmissionOn = MutableLiveData(false)
 
     fun changeTransmissionState(on: Boolean) {
+        if(transmissionOn.value == on) return
+        val frequencyNum: Int
+        try {
+            frequencyNum = frequency.value?.toInt() ?: 0
+        } catch (e: NumberFormatException) {
+            Toast.makeText(getApplication(), "Unusable frequency. Please input a positive number", Toast.LENGTH_LONG).show()
+            return
+        }
+        viewModelScope.launch {
+            if (on) TransmissionApi.postTransmissionStart(frequencyNum) else TransmissionApi.postTransmissionStop()
+        }
         transmissionOn.value = on
     }
-
 }
