1faac837dec695ed8280efea65348b29d0ce180c
SchneckchenAndy <aigner.andi05@gmail.com>
2021-06-07 21:50:16 +0200

    Implement API calls in PlaybackViewModel

--- app/app/src/main/java/me/bickositieff/raspio/ui/playback/PlaybackViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/playback/PlaybackViewModel.kt
@@ -2,27 +2,31 @@ package me.bickositieff.raspio.ui.playback
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import me.bickositieff.raspio.generated.api.PlaybackApi
+import me.bickositieff.raspio.generated.api.TransmissionApi
 
 class PlaybackViewModel : ViewModel() {
     val playbackRunning = MutableLiveData(false)
 
     fun playPause() {
         if (playbackRunning.value!!) {
-            //pause playback API call
+            //Pause API call when implemented
             playbackRunning.value = false
             println("Playback off")
         } else {
-            //start/resume playback API call
+            viewModelScope.launch { PlaybackApi.postPlaybackPlay() }
             playbackRunning.value = true
             println("Playback on")
         }
     }
 
     fun skipSong() {
-        //API call
+        viewModelScope.launch { PlaybackApi.postPlaybackNext() }
     }
 
     fun stopPlayback() {
-        //API call
+        viewModelScope.launch { PlaybackApi.postPlaybackStop() }
     }
 }
