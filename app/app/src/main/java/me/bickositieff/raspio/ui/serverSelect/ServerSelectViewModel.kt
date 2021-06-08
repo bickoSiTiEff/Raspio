96ab7bba1c891334705418e6bf9aa090e3d3e2c5
CallMeAlasca <github@sarah.noebauer.com>
2021-06-08 12:50:17 +0200

    Check IP Syntax

--- app/app/src/main/java/me/bickositieff/raspio/ui/serverSelect/ServerSelectViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/serverSelect/ServerSelectViewModel.kt
@@ -1,23 +1,27 @@
 package me.bickositieff.raspio.ui.serverSelect
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
 import androidx.preference.PreferenceManager
 
 class ServerSelectViewModel(application: Application) : AndroidViewModel(application) {
     val ip = MutableLiveData<String>().apply {
-        value = ""
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplication())
+        value = sharedPrefs.getString("ip", "")
     }
     val valid: LiveData<Boolean> = ip.map { ip ->
         //check IP-syntax
-        true
+        val regex = Regex("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\$")
+        ip.matches(regex)
     }
 
-    fun confirmIP(){
+    fun confirmIP() {
         //check connectivity
-
         val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplication())
-        with(sharedPrefs.edit()){
+        with(sharedPrefs.edit()) {
             putString("ip", ip.value!!)
             apply()
         }
