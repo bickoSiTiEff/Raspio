5cb8041c0a8477e2394bfb4361ae1e7dc4522601
CallMeAlasca <github@sarah.noebauer.com>
2021-06-08 11:46:01 +0200

    Create placeholder for IP validity check

--- app/app/src/main/java/me/bickositieff/raspio/ui/serverSelect/ServerSelectFragment.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/serverSelect/ServerSelectFragment.kt
@@ -1,14 +1,11 @@
 package me.bickositieff.raspio.ui.serverSelect
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import me.bickositieff.raspio.R
-import me.bickositieff.raspio.databinding.FragmentServerControlsBinding
 import me.bickositieff.raspio.databinding.FragmentServerSelectBinding
 
 class ServerSelectFragment : Fragment() {
--- app/app/src/main/java/me/bickositieff/raspio/ui/serverSelect/ServerSelectViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/serverSelect/ServerSelectViewModel.kt
@@ -1,10 +1,21 @@
 package me.bickositieff.raspio.ui.serverSelect
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 
 class ServerSelectViewModel : ViewModel() {
     val ip = MutableLiveData<String>().apply {
         value = ""
+    }
+    val valid: LiveData<Boolean> = ip.map { ip ->
+        //check IP-syntax
+        //check connectivity
+        true
+    }
+
+    fun confirmIP(){
+        //save as shared Pref
     }
 }
