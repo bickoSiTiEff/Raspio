8846871516f5bb83c1bf87c848929f840e6a3e2b
Fabian <fabian.paul.gurtner@gmail.com>
2021-06-14 00:26:26 +0200

    Implement file upload API mock

--- server/src/mockRoutes/library.ts
+++ server/src/mockRoutes/library.ts
@@ -1,5 +1,7 @@
-import {Router} from "express";
+import Router from "express-promise-router";
+import {raw} from "body-parser";
 import {Song} from "../models/Song";
+import {parseBuffer} from "music-metadata";
 
 const library = Router();
 
@@ -20,6 +22,24 @@ export const mockLibrary: Song[] = [
 
 library.get("/", (req, res) => {
 	res.send(mockLibrary);
+});
+
+library.post("/", raw({
+	limit: "1GB",
+	type: "audio/mp3"
+}), async (req, res) => {
+	const meta = await parseBuffer(req.body, "audio/mp3", {
+		skipCovers: true
+	});
+	const duration = Math.round(meta.format.duration as number);
+	console.log(`[MOCK] Uploaded MP3 file of song "${meta.common.title}" from artist "${meta.common.artist}" with a duration of ${duration} seconds.`);
+	mockLibrary.push({
+		path: `${meta.common.artist}/${meta.common.title}.mp3`,
+		title: meta.common.title as string,
+		artist: meta.common.artist as string,
+		duration: duration
+	});
+	res.status(201).send();
 });
 
 export default library;
