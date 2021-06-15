790e7a37275c4760c19d9a9a726f879d542dce84
CallMeAlasca <github@sarah.noebauer.com>
2021-06-15 12:56:16 +0200

    Add polling data from server

--- app/app/src/main/java/me/bickositieff/raspio/ui/playlist/PlaylistViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/playlist/PlaylistViewModel.kt
@@ -3,12 +3,16 @@ package me.bickositieff.raspio.ui.playlist
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.liveData
-import me.bickositieff.raspio.generated.api.PlaylistApi
+import me.bickositieff.raspio.generated.api.LibraryApi
 import me.bickositieff.raspio.ui.models.Song
 
 class PlaylistViewModel(application: Application) : AndroidViewModel(application) {
     val playlist = liveData {
         emit(emptyList())
-        emit(PlaylistApi.getPlaylist().body()!!.map { Song(it.title!!, it.artist!!, it.duration!!, null) })
+        while (true) {
+            emit(emptyList())
+            emit(LibraryApi.getSongs().body()!!.map { Song(it.path!!, it.title!!, it.artist!!, it.duration!!, null) })
+            kotlinx.coroutines.delay(1000)
+        }
     }
 }
