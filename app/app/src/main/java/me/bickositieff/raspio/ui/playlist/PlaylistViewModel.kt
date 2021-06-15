5ffa00e0d2a290efa0e07b423bbfd88e8531d37b
CallMeAlasca <github@sarah.noebauer.com>
2021-06-15 13:46:20 +0200

    Fix using wrong API

--- app/app/src/main/java/me/bickositieff/raspio/ui/playlist/PlaylistViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/playlist/PlaylistViewModel.kt
@@ -3,16 +3,17 @@ package me.bickositieff.raspio.ui.playlist
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.liveData
-import me.bickositieff.raspio.generated.api.LibraryApi
+import me.bickositieff.raspio.generated.api.PlaylistApi
 import me.bickositieff.raspio.ui.models.Song
 
 class PlaylistViewModel(application: Application) : AndroidViewModel(application) {
     val playlist = liveData {
         emit(emptyList())
         while (true) {
-            emit(emptyList())
-            emit(LibraryApi.getSongs().body()!!.map { Song(it.path!!, it.title!!, it.artist!!, it.duration!!, null) })
-            kotlinx.coroutines.delay(1000)
+            emit(
+                PlaylistApi.getPlaylist().body()!!
+                    .map { Song(it.path!!, it.title!!, it.artist!!, it.duration!!, null) })
+            kotlinx.coroutines.delay(5000)
         }
     }
 }
