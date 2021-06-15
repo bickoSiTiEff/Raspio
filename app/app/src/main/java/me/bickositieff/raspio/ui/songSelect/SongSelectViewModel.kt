b80b06ceea67cda21051b92ea81ea087c13f3c56
CallMeAlasca <github@sarah.noebauer.com>
2021-06-15 10:41:08 +0200

    Add song select to nav

--- app/app/src/main/java/me/bickositieff/raspio/ui/songSelect/SongSelectViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/songSelect/SongSelectViewModel.kt
@@ -3,7 +3,6 @@ package me.bickositieff.raspio.ui.songSelect
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import me.bickositieff.raspio.generated.api.LibraryApi
-import me.bickositieff.raspio.generated.api.PlaylistApi
 import me.bickositieff.raspio.ui.models.Song
 
 class SongSelectViewModel : ViewModel() {
