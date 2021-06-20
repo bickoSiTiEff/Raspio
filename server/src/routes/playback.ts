bd24065cabc57f94ab261cb59ed5c419a51ba5ea
Fabian <fabian.paul.gurtner@gmail.com>
2021-06-20 14:49:41 +0200

    Implement new playback APIs

--- server/src/routes/playback.ts
+++ server/src/routes/playback.ts
@@ -13,6 +13,16 @@ export default async (): Promise<ExpressRouter> => {
 		res.status(204).send();
 	});
 
+	playback.post("/pause", async (req, res) => {
+		await mpc.playback.pause();
+		res.status(204).send();
+	});
+
+	playback.post("/previous", async (req, res) => {
+		await mpc.playback.previous();
+		res.status(204).send();
+	});
+
 	playback.post("/next", async (req, res) => {
 		await mpc.playback.next();
 		res.status(204).send();
@@ -20,6 +30,42 @@ export default async (): Promise<ExpressRouter> => {
 
 	playback.post("/stop", async (req, res) => {
 		await mpc.playback.stop();
+		res.status(204).send();
+	});
+
+	playback.post("/seek", async (req, res) => {
+		const time = Number(req.query.time);
+		await mpc.playback.seekCur(time, false);
+		res.status(204).send();
+	});
+
+	playback.post("/consume", async (req, res) => {
+		const consume = Boolean(req.query.consume);
+		await mpc.playbackOptions.setConsume(consume);
+		res.status(204).send();
+	});
+
+	playback.post("/crossfade", async (req, res) => {
+		const amount = Number(req.query.amount);
+		await mpc.playbackOptions.setCrossfade(amount);
+		res.status(204).send();
+	});
+
+	playback.post("/shuffle", async (req, res) => {
+		const shuffle = Boolean(req.query.shuffle);
+		await mpc.playbackOptions.setRandom(shuffle);
+		res.status(204).send();
+	});
+
+	playback.post("/repeat", async (req, res) => {
+		const repeat = Boolean(req.query.repeat);
+		await mpc.playbackOptions.setRepeat(repeat);
+		res.status(204).send();
+	});
+
+	playback.post("/volume", async (req, res) => {
+		const volume = Number(req.query.volume);
+		await mpc.playbackOptions.setVolume(volume);
 		res.status(204).send();
 	});
 
