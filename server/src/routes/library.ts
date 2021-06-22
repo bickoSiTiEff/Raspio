3375fa6bbff863cbed8dd22478c3fc9eaeff1f97
Fabian <fabian.paul.gurtner@gmail.com>
2021-06-22 11:09:36 +0200

    Implement song upload API

--- server/src/routes/library.ts
+++ server/src/routes/library.ts
@@ -2,6 +2,9 @@ import Router from "express-promise-router";
 import {Router as ExpressRouter} from "express";
 import {MPC, Song} from "mpc-js";
 import * as API from "../models/Song";
+import {raw} from "body-parser";
+import {parseBuffer} from "music-metadata";
+import {writeFile} from "fs/promises";
 
 export default async (): Promise<ExpressRouter> => {
 	const library = Router();
@@ -23,6 +26,28 @@ export default async (): Promise<ExpressRouter> => {
 			} as API.Song));
 
 		res.send(songs);
+	});
+
+	library.post("/", raw({
+		limit: "1GB",
+		type: "audio/mp3"
+	}), async (req, res) => {
+		const meta = await parseBuffer(req.body, "audio/mp3", {
+			skipCovers: true
+		});
+
+		if (meta.common.title === undefined || meta.common.artist === undefined || meta.format.duration === undefined) {
+			console.error("Upload is missing metadata!");
+			res.status(500).send();
+			return;
+		}
+
+		const fileName = `/raspio/music/${Math.round(Math.random() * 10000)}.mp3`;
+		console.log(`Saving "${meta.common.title}" to ${fileName}...`);
+
+		await writeFile(fileName, req.body);
+
+		console.log(`Saved ${fileName}.`);
 	});
 
 	return library;
