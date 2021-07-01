f5e32669f2c5d1d1108df62965d59005a94ddc3a
CallMeAlasca <github@sarah.noebauer.com>
2021-07-01 17:10:12 +0200

    Reformat and clean-up code

--- app/app/src/main/java/me/bickositieff/raspio/ui/serverControls/ServerControlsViewModel.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/serverControls/ServerControlsViewModel.kt
@@ -1,7 +1,6 @@
 package me.bickositieff.raspio.ui.serverControls
 
 import android.app.Application
-import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
