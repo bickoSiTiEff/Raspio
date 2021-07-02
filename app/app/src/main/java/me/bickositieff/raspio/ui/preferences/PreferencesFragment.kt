07d897755cae5ee0363377836f9d0ba4c4db7adc
SchneckchenAndy <aigner.andi05@gmail.com>
2021-07-02 02:45:51 +0200

    Add remaining preferences

--- app/app/src/main/java/me/bickositieff/raspio/ui/preferences/PreferencesFragment.kt
+++ app/app/src/main/java/me/bickositieff/raspio/ui/preferences/PreferencesFragment.kt
@@ -1,7 +1,5 @@
 package me.bickositieff.raspio.ui.preferences
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -12,14 +10,16 @@ import me.bickositieff.raspio.R
 class PreferencesFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        val button: Preference? = findPreference(getString(R.string.remove_button))
+        val button: Preference? = findPreference("remove_button")
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
         button?.setOnPreferenceClickListener {
-            val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
             with(sharedPrefs.edit()) {
                 remove("ip")
                 apply()
             }
+            requireActivity().finish()
             true
         }
+        button?.title = button?.title.toString() + " ${sharedPrefs.getString("ip", "")}"
     }
 }
